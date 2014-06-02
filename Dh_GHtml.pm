0f91893d6d6a8e771acd01bea0301bbfae4104a0
dave <dave@0672e23c-c665-4f79-81c1-cc9a95b2c038>
2014-06-02 21:40:55 +0000

    Add GPL comments to all files.
    git-svn-id: svn://lugh/srv/subversion/repositories/fh-tools/DhG@694 0672e23c-c665-4f79-81c1-cc9a95b2c038

--- Dh_GHtml.pm
+++ Dh_GHtml.pm
@@ -4,6 +4,21 @@
 #
 # (c) 2013 David Haworth
 #
+# This file is part of DhG.
+#
+# DhG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DhG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DhG.  If not, see <http://www.gnu.org/licenses/>.
+#
 # $Id$
 
 package Dh_GHtml;
@@ -72,13 +87,13 @@ sub DhG_MultiHtmlCard
 		{
 			$fn = DhG_GetFilename($id);
 
-			if ( $fn =~ m{\/Tidswells\/} )
-			{
-				# Ignoring Tidswells for now.
-				# print STDOUT "DBG: $id: $name - ignored (Tidswell)!\n";
-			}
-			elsif ( $select eq "all" || !DhG_IsPrivate($id) )
-#			if ( $select eq "all" || !DhG_IsPrivate($id) )
+#			if ( $fn =~ m{\/Tidswells\/} )
+#			{
+#				# Ignoring Tidswells for now.
+#				# print STDOUT "DBG: $id: $name - ignored (Tidswell)!\n";
+#			}
+#			elsif ( $select eq "all" || !DhG_IsPrivate($id) )
+			if ( $select eq "all" || !DhG_IsPrivate($id) )
 			{
 				DhG_HtmlCard($id, $select);
 			}
